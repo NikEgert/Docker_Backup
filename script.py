d28f12b34517e7a929939c203cd8fde5ac7d7301
Nik Egert <nege5580@uni.sydney.edu.au>
2024-12-23 17:16:39 +1100

    start

---
+++ script.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+def main(source, destination):
+    
+    pass
+
+
+
+
+if __name__ == "__main__":
+    source = sys.argv[0]
+    destination = sys.argv[1]
+    main(source, destination)
